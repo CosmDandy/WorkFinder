82bcf45ac67671efe7954be0abea1f4585f97f5b
CosmDandy Air <tkondrashin@icloud.com>
2023-04-17 16:26:44 +0300

    +

--- app/main.py
+++ app/main.py
@@ -17,13 +17,16 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(mess
 
 # Настройки драйвера
 chrome_options = Options()
-# chrome_options.add_argument("--disable-extensions")
-# chrome_options.add_argument("--disable-gpu")
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument('--ignore-ssl-errors=yes')
+chrome_options.add_argument('--ignore-certificate-errors')
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.set_capability("browserVersion", "111.0")
 driver = webdriver.Remote(
-    command_executor='http://clickhouse-server:4444/wd/hub',
+    command_executor='http://localhost:4444/wd/hub',
     options=chrome_options
 )
 
